6fcecf0c3e1b3c8af7d1d87c6767470f9eae7b8c
Camille Ingram <camille_d_ingram@live.com>
2025-05-12 13:46:28 -0400

    Structure for DealershipFileManager

--- CarDealership/src/main/java/com/pluralsight/Dealership.java
+++ CarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -8,12 +8,13 @@ public class Dealership {
     private String name = "";
     private String address = "";
     private String phone = "";
-    private ArrayList <Vehicle> inventory = new ArrayList<>();
+    ArrayList <Vehicle> inventory;
 
-    public Dealership(String name, String address, String phone) {
+    public Dealership(String name, String address, String phone, ArrayList<Vehicle> inventory) {
         this.name = name;
         this.address = address;
         this.phone = phone;
+        this.inventory = inventory;
     }
 
     public String getName() {
--- CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,57 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class DealershipFileManager {
+
+
+    public Dealership getDealership() {
+        Dealership dealership = null;
+        ArrayList<Vehicle> inventory = new ArrayList<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("inventory.csv"));
+
+            String line;
+            line = bufferedReader.readLine();
+            String[] dealershipInfo = line.split("\\|");
+            String name = dealershipInfo[0];
+            String address = dealershipInfo[1];
+            String number = dealershipInfo[2];
+            dealership = new Dealership(name, address, number, inventory);
+
+            while((line = bufferedReader.readLine()) != null) {
+                String[] details = line.split("\\|");
+                int vin = Integer.parseInt(details[0]);
+                int year = Integer.parseInt(details[1]);
+                String make = details[2];
+                String model = details[3];
+                String vehicleType = details[4];
+                String color = details[5];
+                int odometer = Integer.parseInt(details[6]);
+                double price = Double.parseDouble(details[7]);
+                Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+                inventory.add(vehicle);
+            }
+
+            bufferedReader.close();
+        }
+        catch(IOException e) {
+            System.out.println("File not found");
+        }
+        return dealership;
+    }
+
+    public void saveDealership(Dealership dealership) {
+        try{
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("inventory.csv"));
+
+
+            bufferedWriter.close();
+
+        }
+        catch(IOException e) {
+            e.getStackTrace();
+        }
+    }
 }
